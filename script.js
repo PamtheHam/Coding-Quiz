b0986cbf81e42a81abd6544bfffe0ae43103896d
Pamela Hammond <hammondpamelac@gmail.com>
2021-06-28 11:26:59 -0400

    added notes on steps to complete project and a variable for the start button

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+// ```
+//  build a timed coding quiz with multiple-choice questions.
+//  have a high scores container that contains all high scores of players hidden
+// timer of 90 seconds
+// do 5 questions each = 20 points for 100 points total possible
+// extended due date to july 7th
+
+// create an array of answer for questions
+// start screen, questions, right and wrong, then timer, then high scores
+
+
+var startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+console.log('Start')
+
+function startGame()
+
+function nextQuestion()
+
+function selectAnswer()
